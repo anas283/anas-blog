7c5bacbd572767c0311a6a413148aacd6f8698bf
Muhammad Anas <muhammadanasbinadam@gmail.com>
2023-05-15 22:59:20 +0800

    update: home page ui

--- src/app/page.js
+++ src/app/page.js
@@ -1,112 +1,73 @@
-import Image from 'next/image'
+import Image from "next/image";
+import CoworkingPreview from "../../public/coworking-space-preview.png";
+import GitHubIcon from "../../public/github.svg";
+import LinkedInIcon from "../../public/linkedin.svg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex max-w-screen-md mx-auto min-h-screen items-center justify-between px-6 py-24 md:px-0 overflow-x-hidden">
+      <div>
+        <h1 className="text-4xl md:text-5xl font-medium">
+          Hey, I'm Anas. <br />
+          Frontend Developer based in Malaysia
+        </h1>
+        <h4 className="text-md text-slate-500 font-normal mt-4">
+          I'm a blazingly fast frontend developer and passionate 
+          about all kinds of interface development. I pride myself
+          in working very fast, without any sacrifice on quality. 
+          I'm available for freelance work!
+        </h4>
+        <div className="flex flex-row">
+          <div className="mt-6">
+            <a 
+              href="mailto:muhammadanasbinadam@gmail.com"
+              target="_blank"
+              className="bg-slate-900 hover:bg-slate-700 text-sm text-white font-medium py-3 px-8 rounded"
+            >
+              Contact
+            </a>
+          </div>
+          <div className="mt-4 ml-3 w-10 h-10 flex justify-center items-center">
+            <a 
+              href="https://github.com/anas283" 
+              target="_blank"
+            >
+              <Image src={GitHubIcon} alt="github" />
+            </a>
+          </div>
+          <div className="mt-4 w-10 h-10 flex justify-center items-center">
+            <a 
+              href="https://www.linkedin.com/in/anas283/" 
+              target="_blank"
+            >
+              <Image src={LinkedInIcon} alt="linkedin" />
+            </a>
+          </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="mt-24">
+          <h3 className="text-2xl font-medium">Featured Work</h3>
+          <div className="flex flex-col md:flex-row mt-4 w-100">
+            <a 
+              href="https://coca-coworking.vercel.app/"
+              target="_blank"
+              className="w-100 h-64 md:w-1/2 mb-10 cursor-pointer md:pr-2"
+            >
+              <div className="h-52 border overflow-hidden rounded-lg">
+                <Image className="object-cover" src={CoworkingPreview} alt="coworking" />
+              </div>
+              <h5 className="font-normal mt-2">
+                Coworking Space Landing Page
+              </h5>
+              <h6 className="font-normal text-slate-400">
+                Next.js
+              </h6>
+            </a>
+            <div className="w-100 h-52 md:w-1/2 mb-10 rounded-lg md:pl-2 border-dashed border-2 border-slate-400 p-4 flex items-center justify-center">
+              <h6 className="text-slate-400">Coming soon</h6>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   )
